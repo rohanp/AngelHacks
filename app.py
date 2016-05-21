9fcb5247ec96bec5df111d18d7f1799a216f424b
Rohan Pandit <pandit.rohan@gmail.com>
2016-05-21 16:08:35 -0400

    added index.html

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request, flash,\
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
