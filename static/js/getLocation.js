f73fd6f862bd199d7f222bdc7c1412bcc8d75e66
ShirazC <shiraz.chokshi@gmail.com>
2016-05-21 16:51:33 -0400

    updated refresh

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -1,14 +1,22 @@
 var x = document.getElementById("demo");
 
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
+        setTimeout(getLocation(), 1000);
+    } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+    var xcoord = [];
+    var ycoord = [];
+
+    xcoord.push(position.coords.latitude);
+    ycoord.push(position.coords.longitude);
+
+    x.innerHTML = "Latitude: " + xcoord +
+    "<br>Longitude: " + ycoord;
 }
