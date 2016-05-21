e608eb507ff57adebd99087ec640081a0913916b
ShirazC <shiraz.chokshi@gmail.com>
2016-05-21 17:14:10 -0400

    added error handler

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -3,8 +3,8 @@ var x = document.getElementById("demo");
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        setTimeout(getLocation(), 1000);
+        navigator.geolocation.getCurrentPosition(showPosition, handleLocationError, {timeout: 10000});
+
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -19,4 +19,23 @@ function showPosition(position) {
 
     x.innerHTML = "Latitude: " + xcoord +
     "<br>Longitude: " + ycoord;
+}
+
+function handleLocationError(error) {
+  switch(error.code){
+  case 0:
+    updateStatus("There was an error while retrieving your location: " +
+                                 error.message);
+  break;
+  case 1:
+  updateStatus("The user prevented this page from retrieving a location.");
+  break;
+  case 2:
+  updateStatus("The browser was unable to determine your location: " +
+                               error.message);
+  break;
+  case 3:
+  updateStatus("The browser timed out before retrieving the location.");
+  break;
+  }
 }
