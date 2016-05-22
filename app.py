414c5c458b75d05ee70bf253b8e4cf832daa985a
Rohan Pandit <pandit.rohan@gmail.com>
2016-05-22 00:29:09 -0400

    added css and js libraries

--- app.py
+++ app.py
@@ -3,23 +3,70 @@ from flask import Flask, render_template, request, flash,\
 
 app = Flask(__name__)
 
+user_db = {}
+salt = "helo"
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'name' in session:
+			return test(*args, **kwargs)
+		else:
+			flash(u'You must login to access this page.')
+			return redirect(url_for('login'))
+	return wrap
+
 @app.route("/")
-def index():
+def info():
+	return render_template("pages/index.html") 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+	form = LoginForm(request.form)
+
 	if request.method == 'GET':
-		return render_template("index.html")
+		return render_template('pages/login.html', form=form)
 
-	elif request.method == "POST":
-		print(request.form['latitude'])
-		return render_template("index.html")
+	if request.method == 'POST':
+		if form.validate() == False:
+			return render_template('pages/login.html', form=form)
+		else:
+			session['name'] = form.name.data.lower()
+			flash(u'Hi {0}!')
 
-@app.route("/sendData", methods=['GET', 'POST'])
+			return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+	if request.method == 'GET':
+		return render_template('forms/register.html', form=form)
+
+	elif request.method == 'POST':
+		if form.validate() == False:
+			return render_template('pages/register.html', form=RegisterForm())
+		else:            
+			user_db[form.email.data] = form.password.data
+
+			session['name'] = newuser.name
+			flash(u'Successfully Registered')
+
+			return redirect(url_for('home'))
+
+
+@app.route("/home")
+def index():
+	return render_template("pages/index.html")
+
+
+@app.route("/sendData", methods=['POST'])
 def send_data():
 
-	if request.method == "POST":
-		print(request.form['latitude'])
-		redirect(url_for('index'))
+	print(request.form['latitude'])
+	redirect(url_for('index'))
 
-	return render_template("main.html")
+	return render_template("layouts/main.html")
 
 
 if __name__ == "__main__":
