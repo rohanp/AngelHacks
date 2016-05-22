7ddf24098b06cc1bb33b551d00d0fc4d07d1eec6
Rohan Pandit <pandit.rohan@gmail.com>
2016-05-21 23:26:44 -0400

    post request working

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -3,6 +3,7 @@ var x = document.getElementById("demo");
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
+
     } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -11,4 +12,19 @@ function getLocation() {
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
+
+    sendData(position)
+
+}
+
+function sendData(position){
+
+	 $.ajax({
+	  type: "POST",
+	  url: '/sendData',
+	  data: position.coords,
+	  success: function(){
+	  	console.log("sent successfully!")
+	  },
+	});
 }
